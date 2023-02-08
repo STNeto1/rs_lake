48d9d0aec64d018f4c2d656b2d719a0f9f49d630
STNeto1 <gfreitasneto18@gmail.com>
2023-02-08 07:47:41 -0300

    data entry

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::net::SocketAddr;
+
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::post, Json, Router};
+use chrono::Local;
+use lake_data::{Data, Entry};
+use serde_json::Value;
+
+mod lake_data;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/:topic", post(handle_data_input));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handle_data_input(
+    Path(topic): Path<String>,
+    Json(input): Json<Value>,
+) -> impl IntoResponse {
+    let data = Data::new_from_input(&input);
+
+    let entry = Entry {
+        topic,
+        timestamp: Local::now().to_rfc3339(),
+        data,
+    };
+
+    return (StatusCode::OK, Json(entry));
+}
